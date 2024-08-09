d04b6b0365c512696cf4b46163a289e67a07be64
sung-silver <jinna0319@gmail.com>
2024-08-09 18:49:03 +0900

    [FEAT] RecruitController - createRecruit 구현

--- wanted/src/main/java/com/example/wanted/controller/RecruitController.java
+++ wanted/src/main/java/com/example/wanted/controller/RecruitController.java
@@ -1,2 +1,37 @@
-package com.example.wanted.controller;public class RecruitController {
+package com.example.wanted.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.wanted.common.vo.RecruitIdVO;
+import com.example.wanted.controller.dto.request.CreateRecruitRequestDTO;
+import com.example.wanted.service.RecruitService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recruit")
+public class RecruitController {
+	private final RecruitService recruitService;
+
+	@PostMapping
+	public ResponseEntity<Void> createRecruit(@RequestBody @Valid CreateRecruitRequestDTO dto) {
+		RecruitIdVO recruitIdVO = recruitService.createRecruit(dto);
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(recruitIdVO.id())
+			.toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
 }
