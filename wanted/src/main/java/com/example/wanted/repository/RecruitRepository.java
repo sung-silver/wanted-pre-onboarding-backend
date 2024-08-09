11a8b272275ba60e227045f3a04de94e79895ac8
sung-silver <jinna0319@gmail.com>
2024-08-09 18:46:35 +0900

    [ADD] RecruitRepository 추가

--- wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
@@ -1,2 +1,9 @@
-package com.example.wanted.repository;public interface RecuruitRepository {
+package com.example.wanted.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.wanted.domain.Recruit;
+
+public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+
 }
