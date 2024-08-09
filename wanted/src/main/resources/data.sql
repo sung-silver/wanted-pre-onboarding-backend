c08d0fbcdb6a63d493faf9224a54ac3d90067d4a
sung-silver <jinna0319@gmail.com>
2024-08-09 23:13:55 +0900

    [CHORE] data.sql에 더미데이터 추가

--- wanted/src/main/resources/data.sql
+++ wanted/src/main/resources/data.sql
@@ -1,5 +1,12 @@
 insert into company(name, nation, location)
 values ('논술메이트', '한국', '역삼');
+insert into company(name, nation, location)
+values ('건빵', '한국', '가좌');
 
 insert into recruit(company_id, content, tech_stack, position, recruitment_bonus)
 values (1, '함께 성장할 개발자를 기다립니다', 'Spring Boot', 'Back-end', 1000000);
+insert into recruit(company_id, content, tech_stack, position, recruitment_bonus)
+values (1, '함께 성장할 개발자를 기다립니다', 'Spring', 'Back-end', 2000000);
+
+insert into recruit(company_id, content, tech_stack, position, recruitment_bonus)
+values (2, 'iOS 네이티브 개발자를 기다립니다', 'Xcode', 'iOS', 1000000);
