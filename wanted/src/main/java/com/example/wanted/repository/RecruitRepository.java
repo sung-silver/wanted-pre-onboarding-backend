c9137ae8fa79e8406a0085a3b88633eeddad1d80
sung-silver <jinna0319@gmail.com>
2024-08-09 23:33:53 +0900

    [FEAT] RecruitRepository - findRecruitsByKeyword 구현

--- wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
@@ -2,7 +2,11 @@ package com.example.wanted.repository;
 
 import static com.example.wanted.common.exception.exceptionType.RecruitException.*;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.wanted.common.exception.ClientException;
 import com.example.wanted.domain.Recruit;
@@ -11,4 +15,7 @@ public interface RecruitRepository extends JpaRepository<Recruit, Long> {
 	default Recruit findByIdOrThrow(final Long recruitId) {
 		return findById(recruitId).orElseThrow(() -> new ClientException(NOT_FOUND_RECRUIT));
 	}
+
+	@Query("SELECT r FROM Recruit r WHERE LOWER(r.company.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(r.position) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(r.techStack) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+	List<Recruit> findRecruitsByKeyword(@Param("keyword") final String keyword);
 }
