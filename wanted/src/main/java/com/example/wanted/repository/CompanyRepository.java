2af3357af915e9c002e9cc4f6e6476f54d1eae08
sung-silver <jinna0319@gmail.com>
2024-08-09 18:45:59 +0900

    [FEAT] CompanyRepository - findByIdOrThrow 구현

--- wanted/src/main/java/com/example/wanted/repository/CompanyRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/CompanyRepository.java
@@ -1,2 +1,14 @@
-package com.example.wanted.repository;public interface CompanyRepository {
+package com.example.wanted.repository;
+
+import static com.example.wanted.common.exception.exceptionType.CompanyException.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.wanted.common.exception.ClientException;
+import com.example.wanted.domain.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+	default Company findByIdOrThrow(Long companyId) {
+		return findById(companyId).orElseThrow(() -> new ClientException(NOT_FOUND_COMPANY));
+	}
 }
