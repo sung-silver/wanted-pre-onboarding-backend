7889f8a668db510cf9087fb7d9c5110b02b6f35a
sung-silver <jinna0319@gmail.com>
2024-08-10 00:51:01 +0900

    [FEAT] RecruitDetailResponseDTO 구현

--- wanted/src/main/java/com/example/wanted/controller/dto/response/RecruitDetailResponseDTO.java
+++ wanted/src/main/java/com/example/wanted/controller/dto/response/RecruitDetailResponseDTO.java
@@ -1,2 +1,36 @@
-package com.example.wanted.controller.dto.response;public record RecruitDetailResponseDTO() {
+package com.example.wanted.controller.dto.response;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.wanted.common.vo.RecruitDetailVO;
+
+public record RecruitDetailResponseDTO(
+	long recruitId,
+	String companyName,
+	String nation,
+	String location,
+	String position,
+	int recruitmentBonus,
+	String techStack,
+	String content,
+	List<Long> otherRecruitIds
+) {
+	public static RecruitDetailResponseDTO from(
+		final RecruitDetailVO recruitDetailVO
+	) {
+		return new RecruitDetailResponseDTO(
+			recruitDetailVO.recruitId(),
+			recruitDetailVO.companyName(),
+			recruitDetailVO.nation(),
+			recruitDetailVO.location(),
+			recruitDetailVO.position(),
+			recruitDetailVO.recruitmentBonus(),
+			recruitDetailVO.techStack(),
+			recruitDetailVO.content(),
+			Optional.ofNullable(recruitDetailVO.otherRecruitIds())
+				.orElse(Collections.emptyList())
+		);
+	}
 }
