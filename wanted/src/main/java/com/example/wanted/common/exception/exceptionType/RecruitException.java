758dd21c15a6443cb78fa421d9ac1a2fdd17a032
sung-silver <jinna0319@gmail.com>
2024-08-09 19:32:42 +0900

    [FEAT] RecruitException - 404 예외 구현

--- wanted/src/main/java/com/example/wanted/common/exception/exceptionType/RecruitException.java
+++ wanted/src/main/java/com/example/wanted/common/exception/exceptionType/RecruitException.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public enum CompanyException implements ExceptionType{
+public enum RecruitException implements ExceptionType {
 	// 404 Not Found
-	NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND, "요청하신 기업을 찾을 수 없습니다");
+	NOT_FOUND_RECRUIT(HttpStatus.NOT_FOUND, "요청하신 채용공고를 찾을 수 없습니다");
 
 	private final HttpStatus status;
 	private final String message;
