b4517242ebd667101e550013f3413fb6215c20bc
sung-silver <jinna0319@gmail.com>
2024-08-09 19:33:35 +0900

    [FEAT] RecruitRepository - findByIdOrThrow 구현

--- wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/RecruitRepository.java
@@ -1,9 +1,14 @@
 package com.example.wanted.repository;
 
+import static com.example.wanted.common.exception.exceptionType.RecruitException.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.wanted.common.exception.ClientException;
 import com.example.wanted.domain.Recruit;
 
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-
+	default Recruit findByIdOrThrow(final Long recruitId) {
+		return findById(recruitId).orElseThrow(() -> new ClientException(NOT_FOUND_RECRUIT));
+	}
 }
