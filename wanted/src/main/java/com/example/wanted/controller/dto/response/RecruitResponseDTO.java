2bd2972763ff87430b65fa3a6f941e009d9f5d8e
sung-silver <jinna0319@gmail.com>
2024-08-09 23:10:23 +0900

    [FEAT] RecruitResponseDTO 구현

--- wanted/src/main/java/com/example/wanted/controller/dto/response/RecruitResponseDTO.java
+++ wanted/src/main/java/com/example/wanted/controller/dto/response/RecruitResponseDTO.java
@@ -1,2 +1,25 @@
-package com.example.wanted.controller.dto.response;public record RecruitResponseDTO() {
+package com.example.wanted.controller.dto.response;
+
+import com.example.wanted.common.vo.RecruitVO;
+
+public record RecruitResponseDTO(
+	Long recruitId,
+	String companyName,
+	String nation,
+	String location,
+	String position,
+	Integer recruitmentBonus,
+	String techStack
+) {
+	public static RecruitResponseDTO from(RecruitVO recruit) {
+		return new RecruitResponseDTO(
+			recruit.recruitId(),
+			recruit.companyName(),
+			recruit.nation(),
+			recruit.location(),
+			recruit.position(),
+			recruit.recruitmentBonus(),
+			recruit.techStack()
+		);
+	}
 }
