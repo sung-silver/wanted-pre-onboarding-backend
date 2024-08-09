1e78cba17d388811a99bed2e2104d0ecfc181078
sung-silver <jinna0319@gmail.com>
2024-08-10 02:27:36 +0900

    [FEAT] ApplyRepository 구현

--- wanted/src/main/java/com/example/wanted/repository/ApplyRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/ApplyRepository.java
@@ -1,2 +1,9 @@
-package com.example.wanted.repository;public interface ApplyRepository {
+package com.example.wanted.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.wanted.domain.Apply;
+
+public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
 }
