ef0170c5094c13203bf91517a27a6f0ad1d2c58a
sung-silver <jinna0319@gmail.com>
2024-08-09 19:34:01 +0900

    [FEAT] UpdateRecruitRequestDTO 구현

--- wanted/src/main/java/com/example/wanted/controller/dto/request/UpdateRecruitRequestDTO.java
+++ wanted/src/main/java/com/example/wanted/controller/dto/request/UpdateRecruitRequestDTO.java
@@ -1,2 +1,8 @@
-package com.example.wanted.controller.dto.request;public record UpdateRecruitRequestDTO() {
+package com.example.wanted.controller.dto.request;
+
+public record UpdateRecruitRequestDTO(
+	String position,
+	Integer recruitmentBonus,
+	String content,
+	String techStack) {
 }
