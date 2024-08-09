920eec2193645000f4d59db8fa01c578bd126141
sung-silver <jinna0319@gmail.com>
2024-08-10 02:27:18 +0900

    [FEAT] MemberRepository - findByIdOrThrow 구현

--- wanted/src/main/java/com/example/wanted/repository/MemberRepository.java
+++ wanted/src/main/java/com/example/wanted/repository/MemberRepository.java
@@ -1,2 +1,14 @@
-package com.example.wanted.repository;public interface MemberRepository {
+package com.example.wanted.repository;
+
+import static com.example.wanted.common.exception.exceptionType.MemberExceptionType.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.wanted.common.exception.ClientException;
+import com.example.wanted.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	default Member findByIdOrThrow(final Long memberId) {
+		return findById(memberId).orElseThrow(() -> new ClientException(NOT_FOUND_MEMBER));
+	}
 }
