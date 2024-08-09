ffeb0c768ea46a409040e653f0d2491e32ef2389
sung-silver <jinna0319@gmail.com>
2024-08-09 23:12:03 +0900

    [FEAT] RecruitController - getRecruits() 구현

--- wanted/src/main/java/com/example/wanted/controller/RecruitController.java
+++ wanted/src/main/java/com/example/wanted/controller/RecruitController.java
@@ -1,9 +1,12 @@
 package com.example.wanted.controller;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.wanted.common.vo.RecruitIdVO;
+import com.example.wanted.common.vo.RecruitVO;
 import com.example.wanted.controller.dto.request.CreateRecruitRequestDTO;
 import com.example.wanted.controller.dto.request.UpdateRecruitRequestDTO;
+import com.example.wanted.controller.dto.response.RecruitResponseDTO;
 import com.example.wanted.service.RecruitService;
 
 import jakarta.validation.Valid;
@@ -22,7 +27,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/recruit")
+@RequestMapping("/api/recruits")
 public class RecruitController {
 	private final RecruitService recruitService;
 
@@ -49,5 +54,14 @@ public class RecruitController {
 	public ResponseEntity<Void> deleteRecruit(@PathVariable final long recruitId) {
 		recruitService.deleteRecruit(recruitId);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping
+	public ResponseEntity<List<RecruitResponseDTO>> getRecruits() {
+		List<RecruitVO> recruits = recruitService.getRecruits();
+		List<RecruitResponseDTO> recruitResponseDTOs = recruits.stream()
+			.map(RecruitResponseDTO::from)
+			.collect(Collectors.toList());
+		return ResponseEntity.ok(recruitResponseDTOs);
 	}
 }
