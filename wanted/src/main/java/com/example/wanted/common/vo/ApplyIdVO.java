94144fb8371f9d42ea76820870938df4afeb00b6
sung-silver <jinna0319@gmail.com>
2024-08-10 02:27:55 +0900

    [FEAT] ApplyIdVO 구현

--- wanted/src/main/java/com/example/wanted/common/vo/ApplyIdVO.java
+++ wanted/src/main/java/com/example/wanted/common/vo/ApplyIdVO.java
@@ -1,2 +1,7 @@
-package com.example.wanted.common.vo;public record ApplyIdVO() {
+package com.example.wanted.common.vo;
+
+public record ApplyIdVO(long id) {
+	public static ApplyIdVO of(long id) {
+		return new ApplyIdVO(id);
+	}
 }
