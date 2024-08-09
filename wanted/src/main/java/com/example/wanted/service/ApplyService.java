c2769c90dbfa1e9817b7408b554255eaff8f02aa
sung-silver <jinna0319@gmail.com>
2024-08-10 02:28:38 +0900

    [FEAT] ApplyService - createApply 구현

--- wanted/src/main/java/com/example/wanted/service/ApplyService.java
+++ wanted/src/main/java/com/example/wanted/service/ApplyService.java
@@ -1,2 +1,32 @@
-package com.example.wanted.service;public class ApplyService {
+package com.example.wanted.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.wanted.common.vo.ApplyIdVO;
+import com.example.wanted.controller.dto.request.CreateApplyRequestDTO;
+import com.example.wanted.domain.Apply;
+import com.example.wanted.domain.Member;
+import com.example.wanted.domain.Recruit;
+import com.example.wanted.repository.ApplyRepository;
+import com.example.wanted.repository.MemberRepository;
+import com.example.wanted.repository.RecruitRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyService {
+	private final ApplyRepository applyRepository;
+	private final MemberRepository memberRepository;
+	private final RecruitRepository recruitRepository;
+
+	@Transactional
+	public ApplyIdVO createApply(CreateApplyRequestDTO request) {
+		Member member = memberRepository.findByIdOrThrow(request.memberId());
+		Recruit recruit = recruitRepository.findByIdOrThrow(request.recruitId());
+		Apply apply = Apply.createApply(member, recruit);
+		applyRepository.save(apply);
+		return ApplyIdVO.of(apply.getId());
+	}
 }
