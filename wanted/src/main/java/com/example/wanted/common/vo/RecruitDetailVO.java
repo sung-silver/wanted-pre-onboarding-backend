dde4c1b2b52f52d7a6031979bc2f9cab832bf7db
sung-silver <jinna0319@gmail.com>
2024-08-10 00:51:19 +0900

    [FEAT] RecruitDetailVO 구현

--- wanted/src/main/java/com/example/wanted/common/vo/RecruitDetailVO.java
+++ wanted/src/main/java/com/example/wanted/common/vo/RecruitDetailVO.java
@@ -1,2 +1,39 @@
-package com.example.wanted.common.vo;public record RecruitDetailVO() {
+package com.example.wanted.common.vo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.wanted.domain.Recruit;
+
+public record RecruitDetailVO(
+	long recruitId,
+	String companyName,
+	String nation,
+	String location,
+	String position,
+	int recruitmentBonus,
+	String techStack,
+	String content,
+	List<Long> otherRecruitIds
+) {
+	public static RecruitDetailVO from(
+		final Recruit recruit
+	) {
+		return new RecruitDetailVO(
+			recruit.getId(),
+			recruit.getCompany().getName(),
+			recruit.getCompany().getNation(),
+			recruit.getCompany().getLocation(),
+			recruit.getPosition(),
+			recruit.getRecruitmentBonus(),
+			recruit.getTechStack(),
+			recruit.getContent(),
+			Optional.ofNullable(recruit.getCompany().getRecruitList())
+				.orElse(Collections.emptyList())
+				.stream()
+				.map(Recruit::getId)
+				.filter(id -> !id.equals(recruit.getId()))
+				.toList());
+	}
 }
