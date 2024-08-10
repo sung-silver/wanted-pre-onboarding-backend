db7cba71dc56dfec1ac22fc606b2a1731ff5aa7d
sung-silver <jinna0319@gmail.com>
2024-08-11 01:47:38 +0900

    [TEST] Apply 엔티티 - createApplyTest 작성

--- wanted/src/test/java/com/example/wanted/domain/ApplyTest.java
+++ wanted/src/test/java/com/example/wanted/domain/ApplyTest.java
@@ -1,4 +1,29 @@
+package com.example.wanted.domain;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.example.wanted.Fixture.MemberFixture;
+import com.example.wanted.Fixture.RecruitFixture;
+
+@ActiveProfiles("test")
 class ApplyTest {
-  
+
+	@Test
+	@DisplayName("Apply를 생성할 수 있다")
+	void createApplyTest() {
+		// given
+		Member member = MemberFixture.createMember();
+		Recruit recruit = RecruitFixture.createRecruit();
+
+		// when
+		Apply apply = Apply.createApply(member, recruit);
+
+		// then
+		assertEquals(apply.getMember(), member);
+		assertEquals(apply.getRecruit(), recruit);
+	}
 }
