62d414268190013d7a6e41ac5f37bc164010dd9b
sung-silver <jinna0319@gmail.com>
2024-08-11 01:42:50 +0900

    [TEST] CompanyFixture 구현

--- wanted/src/test/java/com/example/wanted/Fixture/CompanyFixture.java
+++ wanted/src/test/java/com/example/wanted/Fixture/CompanyFixture.java
@@ -1,2 +1,14 @@
-package com.example.wanted.Fixture;public class CompanyFixture {
+package com.example.wanted.Fixture;
+
+import com.example.wanted.domain.Company;
+
+public class CompanyFixture {
+	public static final String NAME = "companyName";
+	public static final String NATION = "companyNation";
+	public static final String LOCATION = "companyLocation";
+
+	public static Company createCompany() {
+		return Company.createCompany(NAME, NATION, LOCATION);
+	}
+
 }
