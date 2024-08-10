601a0d6c5bfd00a4f3e7d48932c9a970c8942c1f
sung-silver <jinna0319@gmail.com>
2024-08-11 02:02:06 +0900

    [TEST] Company 엔티티 - createCompanyTest 작성

---
+++ wanted/src/test/java/com/example/wanted/domain/CompanyTest.java
@@ -0,0 +1,30 @@
+package com.example.wanted.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CompanyTest {
+	String name;
+	String nation;
+	String location;
+
+	@Test
+	@DisplayName("Company를 생성할 수 있다")
+	void createCompanyTest() {
+		// given
+		name = "wanted";
+		nation = "korea";
+		location = "seoul";
+
+		// when
+		Company company = Company.createCompany(name, nation, location);
+
+		// then
+		assertEquals(company.getName(), name);
+		assertEquals(company.getNation(), nation);
+		assertEquals(company.getLocation(), location);
+	}
+
+}
