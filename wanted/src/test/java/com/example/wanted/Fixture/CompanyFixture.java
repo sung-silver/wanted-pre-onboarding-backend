e8bdb47cf45fb91a9ee01f588a955ff197b83838
sung-silver <jinna0319@gmail.com>
2024-08-11 03:09:47 +0900

    [DEL] create 메서드 삭제

--- wanted/src/test/java/com/example/wanted/Fixture/CompanyFixture.java
+++ wanted/src/test/java/com/example/wanted/Fixture/CompanyFixture.java
@@ -1,14 +1,8 @@
 package com.example.wanted.Fixture;
 
-import com.example.wanted.domain.Company;
-
 public abstract class CompanyFixture {
 	public static final String NAME = "companyName";
 	public static final String NATION = "companyNation";
 	public static final String LOCATION = "companyLocation";
-
-	public static Company createCompany() {
-		return Company.createCompany(NAME, NATION, LOCATION);
-	}
 
 }
