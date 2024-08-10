eddbdbdc9c51bef5fdb462783861f50d445f7be9
sung-silver <jinna0319@gmail.com>
2024-08-11 01:41:04 +0900

    [DEL] Member_id 삭제

--- wanted/src/test/java/com/example/wanted/Fixture/MemberFixture.java
+++ wanted/src/test/java/com/example/wanted/Fixture/MemberFixture.java
@@ -3,7 +3,6 @@ package com.example.wanted.Fixture;
 import com.example.wanted.domain.Member;
 
 public class MemberFixture {
-	public static final Long MEMBER_ID = 1L;
 	public static final String MEMBER_EMAIL = "memberEmail";
 	public static final String MEMBER_NAME = "memberName";
 	public static final String MEMBER_PASSWORD = "memberPassword";
