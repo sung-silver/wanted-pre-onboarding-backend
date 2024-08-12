fdb82045c49b9d3ae2e36f34bbbe69787cc26377
sung-silver <jinna0319@gmail.com>
2024-08-12 22:27:24 +0900

    [TEST] MemberRepositoryTest - findByIdOrThrowWithNotExistId 작성

--- wanted/src/test/java/com/example/wanted/repository/MemberRepositoryTest.java
+++ wanted/src/test/java/com/example/wanted/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.wanted.repository;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.wanted.Fixture.MemberFixture;
+import com.example.wanted.common.exception.ClientException;
+import com.example.wanted.common.exception.exceptionType.MemberExceptionType;
 import com.example.wanted.domain.Member;
 
 @ActiveProfiles("test")
@@ -30,6 +33,17 @@ class MemberRepositoryTest {
 
 		// then
 		assertEquals(foundMember, member);
+	}
+
+	@Test
+	@DisplayName("없는 id로 Member를 조회할 경우 예외가 발생한다")
+	void findByIdOrThrowWithNotExistId() {
+		// given
+		Long notExistId = 0L;
+
+		// when, then
+		assertThat(assertThrows(ClientException.class, () -> memberRepository.findByIdOrThrow(notExistId))
+			.getExceptionType()).isEqualTo(MemberExceptionType.NOT_FOUND_MEMBER);
 	}
 
 }
