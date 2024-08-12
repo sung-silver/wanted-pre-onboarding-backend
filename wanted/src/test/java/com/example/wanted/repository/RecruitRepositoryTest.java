a15193cab45890a277517bb9be489ac2ee902179
sung-silver <jinna0319@gmail.com>
2024-08-12 22:51:13 +0900

    [TEST] RecruitRepositoryTest - findByIdOrThrow 작성

---
+++ wanted/src/test/java/com/example/wanted/repository/RecruitRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.wanted.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.example.wanted.Fixture.CompanyFixture;
+import com.example.wanted.Fixture.RecruitFixture;
+import com.example.wanted.domain.Company;
+import com.example.wanted.domain.Recruit;
+
+@ActiveProfiles("test")
+@DataJpaTest
+class RecruitRepositoryTest {
+	@Autowired
+	private RecruitRepository recruitRepository;
+
+	@Autowired
+	private CompanyRepository companyRepository;
+
+	@Test
+	@DisplayName("Recruit를 조회할 수 있다")
+	void findByIdOrThrowTest() {
+		// given
+		Company company = Company.createCompany(CompanyFixture.NAME, CompanyFixture.NATION, CompanyFixture.LOCATION);
+		companyRepository.save(company);
+		Recruit recruit = Recruit.createRecruit(RecruitFixture.POSITION, RecruitFixture.RECRUITMENT_BONUS,
+			RecruitFixture.TECH_STACK, RecruitFixture.CONTENT, company);
+		recruitRepository.save(recruit);
+
+		// when
+		Recruit foundRecruit = recruitRepository.findByIdOrThrow(recruit.getId());
+
+		// then
+		assertEquals(foundRecruit, recruit);
+	}
+
+}
